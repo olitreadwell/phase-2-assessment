5f4c61d55417f77f84c89ec7bd570b0fee8f8c49
Kevin Solorio <kevin@detone8.com>
2014-03-25 17:12:54 -0500

    update student migration with image url

--- db/migrate/20140325155352_create_students.rb
+++ db/migrate/20140325155352_create_students.rb
@@ -3,6 +3,7 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+      t.string :image_url
       t.string :email
       t.string :password_digest
       t.text   :bio
