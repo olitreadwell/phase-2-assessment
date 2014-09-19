77f7bb6aef74cb0020c57022604ab373b3e863e5
Oli Treadwell <olitreadwell@gmail.com>
2014-09-19 15:42:47 -0700

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
         console.log("success");
         $("#create_org").html("<h2>Create new Organization</h2><form class='form' action='/organizations/new' method='post'><label>Name:<input type='text' name='organization_name' placeholder='your org's name'><br></label><label>Description:<textarea name='organization_description' id=' cols='30' rows='10' placeholder='your org's info'></textarea><br></label><input id='ajax_create' type='submit' value='Create'></form>'");
     });//create org click
-    $( "#create_org").click("#ajax_create", function(event) {
+    $( "#ajax_create").submit("#ajax_create", function(event) {
                $.ajax({
             type: "POST",
             url: "/organizations/new",
-            data: ,
+            data: {},
             dataType: "json"
         })//ajax-end
             .done(function(){
@@ -26,3 +26,5 @@ $(document).ready(function() {
           });//event-handler end
     });//document ready end
 // })
+// on the ajax_create submit begin an ajax post to the url included
+    // the data will be the values of the fields
